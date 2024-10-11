04d9695f27e70b5a9ffa5cba05642b42afb4a9b8
Aarpha <cheeseball99@proton.me>
2024-10-11 11:25:41 -0400

    added more stuff

---
+++ .d/autons/autonRoute.cpp.d
@@ -0,0 +1,4 @@
+bin/autons/autonRoute.cpp.o: src/autons/autonRoute.cpp \
+ src/autons/autonlib.hpp src/autons/pid.hpp
+src/autons/autonlib.hpp:
+src/autons/pid.hpp:
---
+++ .d/autons/auton_route.cpp.d
@@ -0,0 +1,4 @@
+bin/autons/auton_route.cpp.o: src/autons/auton_route.cpp \
+ src/autons/autonlib.hpp src/autons/pid.hpp
+src/autons/autonlib.hpp:
+src/autons/pid.hpp:
