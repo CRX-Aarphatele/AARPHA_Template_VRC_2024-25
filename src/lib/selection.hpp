d5513ea2653d1765f875d9d9810ca18147a25d74
Aarpha <cheeseball99@proton.me>
2024-10-28 10:40:59 -0400

    hello

--- src/lib/selection.hpp
+++ src/lib/selection.hpp
@@ -7,6 +7,7 @@
 #include "liblvgl/misc/lv_area.h"
 #include "liblvgl/misc/lv_color.h"
 #include "liblvgl/misc/lv_style.h"
+#include "liblvgl/widgets/lv_btnmatrix.h"
 #include "liblvgl/widgets/lv_label.h"
 #include "liblvgl/widgets/lv_switch.h"
 #include "pros/colors.h"
@@ -21,70 +22,124 @@
 Selection Box Stuff
 
 */
-
+ 
 const inline int COL_SIZE = 160;
 const inline int ROW_SIZE = 34;
 
 const inline int BTN_PADDING = 2;
 const inline int BTN_TXT_LEFT_PADDING = 5;
 
-inline lv_style_t style_trestle;
+namespace selector {
 
-static void style_init (void){
-    
+    inline lv_style_t style_trestle;
 
-    lv_style_init(&style_trestle);
-    
-    lv_style_set_bg_opa(&style_trestle, LV_OPA_COVER);
+    inline static const char * map[] = {};
 
-}
+    static void style_init (void){
+        
 
-class button {
-    public:
-        int x,y;
-        int width,height;
-        int padding;
+        lv_style_init(&style_trestle);
 
-        lv_color_t backgroundColour, highlightColour, hoverColour, clickColour;
-        lv_style_t * btnStyle;\
-        std::string text;
+        /*
+        403C40
+        9C919C
+        FFF2FE
+        9D1FA8
+        deae21
+        */
 
-        bool selected;
+        lv_color_t manganese_dark = LV_COLOR_MAKE16(8,15,8); // ourple
+        lv_color_t manganese_clouds = LV_COLOR_MAKE(19,33,19); // ourple
+        lv_color_t wisteria = LV_COLOR_MAKE16(31, 54, 31); // ourple
+        lv_color_t porrum_dolce = LV_COLOR_MAKE16(15,8,13); // ourple
+        lv_color_t saphris_p = LV_COLOR_MAKE16(17,33,2); // yellow
 
-        button( int px, int py,
-                int w, int h, 
-                lv_color_t bgColour, lv_color_t hlColour, 
-                lv_color_t hvColour, lv_color_t ckColour, 
-                std::string txt, bool sel = false)
+        lv_color_t teamBtn_tx_p = lv_color_black();
 
-                : x(px), y(py)
-                , width(w), height(h)
-                , backgroundColour(bgColour), highlightColour(hlColour)
-                , hoverColour(hvColour), clickColour(ckColour)
-                , text(txt) 
-                , selected(sel)
-                {};
-};
+        lv_color_t blueTeam_hl = LV_COLOR_MAKE16(16,28,29);
+        lv_color_t blueTeam_ll = LV_COLOR_MAKE16(7,7,13);
 
-/*
+        lv_color_t redTeam_hl = LV_COLOR_MAKE16(26,11,14);
+        lv_color_t redTeam_ll = LV_COLOR_MAKE16(14,4,10);
 
-Selections
 
-*/
+        
+        lv_style_set_bg_opa(&style_trestle, LV_OPA_COVER);
+        lv_style_set_bg_color(&style_trestle, manganese_clouds);
+        
 
-enum autonEnum {
-    NOTHING = 0,
-    DEFENSE = 1,
-    OFFENSE = 2,
+    }
 
-    SOLO_AWP_A = -1,
-    SOLO_AWP_B = -2,
-    SOLO_AWP_C = -3
-};
-inline int autonSelect = NOTHING;
+    class toggle_team {
 
-inline void initSelectorMenu(){
-    std::vector<button> e;
-    
-    button mikeel = button(20,20, 200,50,lv_color_hex(0x403C40),lv_color_hex(0xFFF2FE),lv_color_hex(0x9c919c),lv_color_make(90,85,30),"h");
+    };
+
+    class button_slide {
+        public:
+            int x,y;
+            int width,height;
+            int padding;
+            int buttons;
+
+            int colour;
+
+            int btnW, btnH;
+
+            lv_color_t backgroundColour, highlightColour, hoverColour, clickColour;
+            lv_style_t * style_trestle;
+
+            button_slide( int px, int py,
+                    int w, int h,
+                    int btns)
+
+                    : x(px), y(py)
+                    , width(w), height(h),
+                    buttons(btns)
+                    {};
+
+            void init(){
+                btnW = width;
+                btnH = height/buttons;
+
+                style_init();
+
+                
+            }
+
+            void draw(){
+                
+            }
+
+            void callback(lv_event_t * e){
+                lv_obj_t * obj = lv_event_get_target(e);
+                uint32_t id = lv_btnmatrix_get_selected_btn(obj);
+
+            }
+
+    void event_select(lv_event_t * e){
+        
+    }
+    };
+
+    /*
+
+    Selections 
+
+    */
+
+    enum autonEnum {
+        NOTHING = 0,
+        DEFENSE = 1,
+        OFFENSE = 2,
+
+        SOLO_AWP_A = -1,
+        SOLO_AWP_B = -2,
+        SOLO_AWP_C = -3
+    };
+    inline int autonSelect = NOTHING;
+
+    inline void initSelectorMenu(){
+
+    }
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,6 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -31,10 +22,6 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
 	initSelectorMenu();
 }
 
